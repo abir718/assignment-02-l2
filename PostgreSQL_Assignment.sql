1885d7cee72bb1e8886efbb8d4db86437a4ecab9
Abir Hasan <abir.hasan2003s@gmail.com>
2025-05-26 23:27:44 +0600

    rest problem solves

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -46,6 +46,43 @@ INSERT INTO rangers (name, region) VALUES ('Derek Fox', 'Coastal Plains');
 
 SELECT COUNT(DISTINCT species_id) AS unique_species_count FROM sightings;
 
--- Problem 3
+-- Problem 3 
 
 SELECT * FROM sightings WHERE location ILIKE '%Pass%';
+
+-- Problem 4 --
+
+SELECT rangers.name, COUNT(sightings.sighting_id) AS total_sightings FROM rangers
+LEFT JOIN sightings ON rangers.ranger_id = sightings.ranger_id
+GROUP BY rangers.name;
+
+-- Problem 5 -- 
+
+SELECT species.common_name FROM species
+LEFT JOIN sightings ON species.species_id = sightings.species_id
+WHERE sightings.species_id IS NULL;
+
+-- Problem 6 --  
+
+SELECT species.common_name, sightings.sighting_time, rangers.name FROM sightings
+JOIN species ON sightings.species_id = species.species_id
+JOIN rangers ON sightings.ranger_id = rangers.ranger_id
+ORDER BY sightings.sighting_time DESC LIMIT 2; 
+
+-- Problem 7 --
+
+UPDATE species SET conservation_status = 'Historic' WHERE discovery_date < '1800-01-01';
+
+-- Problem 8 --
+
+SELECT sighting_id,
+  CASE 
+    WHEN EXTRACT(HOUR FROM sighting_time) < 12 THEN 'Morning'
+    WHEN EXTRACT(HOUR FROM sighting_time) < 17 THEN 'Afternoon'
+  ELSE 'Evening'
+  END AS time_of_day
+FROM sightings;
+
+-- Problem 9 --
+
+DELETE FROM rangers WHERE ranger_id NOT IN (SELECT DISTINCT sightings.ranger_id FROM sightings);
